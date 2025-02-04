1b7d8e8978cd1b115f40e09ca41f971720fa5631
Jevon Putra <jevonop88@gmail.com>
2025-02-04 23:28:34 +0700

    Implementation splash screen

---
+++ app/src/main/java/com/jop/testvascomm/view/splash/SplashFragment.kt
@@ -0,0 +1,39 @@
+package com.jop.testvascomm.view.splash
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.jop.testvascomm.R
+import com.jop.testvascomm.data.local.AppData
+import com.jop.testvascomm.databinding.FragmentSplashBinding
+import com.jop.testvascomm.ui.BaseFragment
+import com.jop.testvascomm.view.MainActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashFragment : BaseFragment() {
+    private lateinit var binding: FragmentSplashBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentSplashBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(1500)
+            appData.getToken().let {
+                if(it.isNullOrEmpty()){
+                    mainAct.navController.navigate(R.id.action_splashFragment_to_loginFragment)
+                } else {
+                    mainAct.navController.navigate(R.id.action_splashFragment_to_homeFragment)
+                }
+            }
+        }
+    }
+}
